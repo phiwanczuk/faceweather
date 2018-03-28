dbd86ba7b2006a780e3be4184ce0e2da0417c94d
Piotrek <phiwanczuk@gmail.com>
2018-03-28 19:05:16 +0200

    icon display being added

--- src/Components/Weather/Weather.js
+++ src/Components/Weather/Weather.js
@@ -10,10 +10,13 @@ class Weather extends React.Component {
     }
 
 
+
+
+
     fetchData = (event) => {
         event.preventDefault()
         let location = encodeURIComponent(this.state.location)
-        fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=209c0c18886d791519db4cc4308daa95`)
+        fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=209c0c18886d791519db4cc4308daa95&units=metric`)
             .then(response => response.json())
             .then(
                 weather => this.setState({weather})
@@ -27,6 +30,11 @@ class Weather extends React.Component {
     }
 
     render() {
+
+
+
+        // const src = `//openweathermap.org/img/w/${data.weather[0].icon}.png`;
+
         const {weather} = this.state
         return (
             <div className='weather-view'>
@@ -44,8 +52,13 @@ class Weather extends React.Component {
                 </form>   {
                 weather &&
                 <React.Fragment>
+                    <p>City: {weather.name}</p>
+                    <p>City: {weather.weather[0].icon + '.png'}</p>
                     <p>Temperature: {weather.main.temp}</p>
-                    <p>Temperature: {weather.main.humidity}</p>
+                    <p>Humidity: {weather.main.humidity}</p>
+                    {/*<p><img src='http://openweathermap.org/img/w/" + weather[0].icon + ".png' alt='Icon depicting current weather.'/>*/}
+                    {/*</p>*/}
+
                 </React.Fragment>
 
                 }
